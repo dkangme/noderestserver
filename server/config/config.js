e21abd0c5a8cc3bbdfa15d503149824c9911156f
Dennis Kangme <denniskangme@MacBook-Pro-de-Dennis-Kangme.local>
2019-10-13 19:09:30 -0300

    Rest Server-Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 //
 process.env.PORT = process.env.PORT || 3000;
+
+//
+// Entorno
+//
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//
+// Base de datos
+//
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb+srv://dbUser:Mies2019!@cluster0-500f0.gcp.mongodb.net/cafe?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true';
+} else {
+    urlDB = 'mongodb+srv://dbUser:Mies2019!@cluster0-500f0.gcp.mongodb.net/cafe?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true';
+}
+
+process.env.URLDB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,161 @@
+const express = require('express')
+const app = express()
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+
+app.get('/usuario', function(req, res) {
+
+    let desde = req.query.desde || 0;
+    let limite = req.query.limite || 5;
+
+    desde = Number(desde);
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email rol estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                })
+            }
+
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    cuantos: conteo,
+                    usuarios
+                });
+            })
+
+
+        })
+
+
+})
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        rol: body.rol
+    });
+
+
+    usuario.save((error, usuarioDB) => {
+
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                error
+            });
+        }
+
+        // usuarioDB.password = null;
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    })
+
+})
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'rol', 'estado']);
+
+
+    Usuario.findByIdAndUpdate(id, body, {
+        new: true,
+        runValidators: true
+    }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+
+    })
+})
+
+app.delete('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    let body = req.body;
+
+    body.estado = false;
+
+    Usuario.findByIdAndUpdate(id, body, {
+        new: true,
+        runValidators: true
+    }, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (usuarioBorrado === null) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Usuario no encontrado'
+            });
+
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    })
+
+})
+
+// app.delete('/usuario/:id', function(req, res) {
+
+//     let id = req.params.id;
+//     Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+//         if (err) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 err
+//             });
+//         }
+
+//         if (usuarioBorrado === null) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 message: 'Usuario no encontrado'
+//             });
+
+//         }
+//         res.json({
+//             ok: true,
+//             usuario: usuarioBorrado
+//         });
+
+//     })
+
+// })
+
+module.exports = app;
