6dd4026a1408b30381b93988a8a6b29c7423ee4e
Dennis Kangme <denniskangme@MacBook-Pro-de-Dennis-Kangme.local>
2019-10-13 19:50:26 -0300

    Correccion Env MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb+srv://dbUser:7a7rK2W9tiKej98F@cluster0-500f0.gcp.mongodb.net/cafe?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true';
 } else {
-    urlDB = process.env.MOGO_URI;
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
